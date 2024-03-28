c2b43e85fdad78162832481fbadeb08032ad8052
HanseongBae <bha4388@naver.com>
2024-03-29 08:11:27 +0900

    fix : 이동 거리를 생성자로 받아야하는데 잘못 생각하여 연료를 생성자로 받음 수정

---
+++ src/main/java/study/fuel/abstractclazz/Distance.java
@@ -0,0 +1,33 @@
+package study.fuel.abstractclazz;
+
+
+import java.util.Objects;
+
+public class Distance {
+
+    private final int distance;
+
+    public Distance(final int distance) {
+        validateDistance(distance);
+        this.distance = distance;
+    }
+
+    private void validateDistance(final int distance) {
+        if (distance <= 0) {
+            throw new IllegalArgumentException("이동 거리는 0이하의 숫자를 가질 수 없습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Distance distance1 = (Distance) o;
+        return distance == distance1.distance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
+    }
+}
--- src/test/java/study/fuel/abstractclazz/DistanceTest.java
+++ src/test/java/study/fuel/abstractclazz/DistanceTest.java
@@ -5,29 +5,29 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class EfficiencyTest {
+class DistanceTest {
 
     @Test
-    void 원시값인_연비를_포장한_클래스() {
+    void 원시값인_거리를_포장한_클래스() {
         // given
-        Efficiency efficiency = new Efficiency(1);
+        Distance distance = new Distance(1);
 
         // when
-        boolean isEqual = efficiency.equals(new Efficiency(1));
+        boolean isEqual = distance.equals(new Distance(1));
 
         // then
         assertThat(isEqual).isTrue();
     }
 
     @Test
-    void 연비는_0이하의_숫자를_가질_수_없다() {
+    void 거리는_0이하의_숫자를_가질_수_없다() {
         // given
         // when
         // then
         assertThatThrownBy(() -> {
-            new Efficiency(0);
+            new Distance(0);
         }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("연비는 0이하의 숫자를 가질 수 없습니다.");
+                .hasMessage("이동 거리는 0이하의 숫자를 가질 수 없습니다.");
     }
 
 }
